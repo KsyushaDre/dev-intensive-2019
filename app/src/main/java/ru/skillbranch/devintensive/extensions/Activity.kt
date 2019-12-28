f85206cafb3f76a7154c4ef747411177566eff97
Ксения Дребенчук <ksyusha.dre@gmail.com>
2019-12-28 12:37:22 +0300

    hometask_4 take_11

--- app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
+++ app/src/main/java/ru/skillbranch/devintensive/extensions/Activity.kt
@@ -49,6 +49,6 @@ fun Context.dpToPx(dp: Int): Float {
     return dp.toFloat() * this.resources.displayMetrics.density
 }
 
-fun Context.pxToDp (@Px px: Float): Int {
+fun Context.pxToDp (px: Float): Int {
     return (px/this.resources.displayMetrics.density).toInt()
 }
